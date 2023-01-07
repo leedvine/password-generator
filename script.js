537178038afccb0d7103b5061d2b9c4127529365
Lee Vine <leedvine@gmail.com>
2023-01-07 00:08:44 +0000

    sudo code the JS file with thoughts and steps needed to achieve password generation

--- script.js
+++ script.js
@@ -88,23 +88,38 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// After hours of mindless Googling the below must be best achieved via Javascript prompt() method
+
+// Using alerts and possibly document.write() to inform user of choice and errors
+
 // Function to prompt user for password options
 function getPasswordOptions() {
 
 }
+
+//The below most likely acheived by math object. Not sure of security using this in regards to
+// password generation but it should achieve task. Should target specific arrays to pull random charachters
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
 }
 
+// The below should combine all random selections from multiple arrays in 
+// to one and create as a string
+
 // Function to generate password with user input
 function generatePassword() {
 
 }
 
+//not sure of the below statement at this point
+
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
+
+
+//Presnt the user with the password (see string created 2 steps above)
 
 // Write password to the #password input
 function writePassword() {
@@ -113,6 +128,8 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+//This is the event (click) that will ultimately present the user with the password
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
